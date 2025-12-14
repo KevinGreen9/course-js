11a5aba71424886d3930e53bd47ce6682a53a4ba
IvcencoSemion <cema.56.s@mail.ru>
2025-12-14 12:25:49 +0200

    Update 03.js

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -3,34 +3,31 @@
  * Балл может быть целым числом от 0 до 100, а оценки 'F' (0 - 49), 'D' (50 - 69), 'C' (70-79), 'B' (80 - 89), 'A' (90 - 100)
  * Результат сохраните в переменной grade.
  */
+const score = 100 // тестовое значение, можно изменять
+// your code
 
- const score = 100 // тестовое значение, можно изменять
-    // your code
-    let count = 0
-    
-while (count < 5) {
-    let grade = Math.floor(Math.random() * score);
+
+    let grade =  score
 
     if (grade <= 49) {
         grade = 'F'
-        count++
 
     } else if (grade <= 69) {
         grade = 'D'
-        count++
 
     } else if (grade <= 79) {
      grade = 'C'
-        count++
+
 
     } else if (grade <= 89) {
         grade = 'B'
-        count++
+
 
     } else if (grade <= 100) {
         grade = 'A'
-        count++
+
 
     }
 
+console.log(grade)
 }

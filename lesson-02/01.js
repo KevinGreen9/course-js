414ecc2f2346b689b91df1b73e8ba07545ebc856
Golden_boy <cema.56.s@mail.ru>
2025-12-15 21:44:29 +0200

    rfc

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -16,8 +16,8 @@ const hasTemporaryPass = 'user'
 let Access = prompt("Ваш уровень доступа!?", '');
 let pass = prompt("У вас есть специальное разрешение?", '')
 
-const role = (Access === isAdmin) || (Access === isVerifiedUser);
-const permission = (pass === hasTemporaryPass) || (pass === hasSpecialPermission);
+const role = (Access === isAdmin) || (pass === hasSpecialPermission);
+const permission = (Access === isVerifiedUser) || (pass === hasTemporaryPass);
 let isAccess = role && permission
 if (isAccess) {
     console.log(`Hello ${Access} you have a ${pass}`);

f4325a2aa9f92cdb0306d782da3b6503fb9fb61d
Golden_boy <cema.56.s@mail.ru>
2025-12-16 17:55:42 +0200

    f

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -25,9 +25,9 @@
 
 let dice1 = Math.floor(Math.random() * 6) + 1// your code
 let dice2 = Math.floor(Math.random() * 6) + 1// your code
-let isWinningDouble = (dice1 === dice2);// your code
+let isWinningDouble = (dice1 === dice2) && (dice1 > 3);// your code
 
-if(( dice1 > 3) && (dice2 > 3) && (dice2 === dice1)){
+if(( dice1 > 3) && (dice2 > 3) && (dice1 === dice2)){
     console.log('Первый бросок: ' + dice1)
     console.log('Второй бросок: ' + dice2)
 }

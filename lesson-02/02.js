c2317f962159f5da60603ec6f7109b0bf122174e
Golden_boy <cema.56.s@mail.ru>
2025-12-16 17:13:41 +0200

    02

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,16 +7,34 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 // your code
-let dice2 // your code
+// let dice1 = Math.floor(Math.random() * 6)// your code
+// let dice2 = Math.floor(Math.random() * 6)// your code
+// let isWinningDouble // your code
+//
+// if(( dice1 > 3) && (dice2 > 3) && (dice2 === dice1)){
+//     isWinningDouble = dice1 && dice2
+//     console.log('Первый бросок: ' + dice1)
+//     console.log('Второй бросок: ' + dice2)
+// }
+//
+// if (isWinningDouble) {
+//   console.log('Выигрышный дубль!')
+// } else {
+//   console.log('Не выигрышный дубль.')
+// }
+
+let dice1 = Math.floor(Math.random() * 7)// your code
+let dice2 = Math.floor(Math.random() * 7)// your code
 let isWinningDouble // your code
 
-/*
-console.log('Первый бросок: ' + dice1)
-console.log('Второй бросок: ' + dice2)
-if (isWinningDouble) {
-  console.log('Выигрышный дубль!')
-} else {
-  console.log('Не выигрышный дубль.')
+if(( dice1 > 3) && (dice2 > 3) && (dice2 === dice1)){
+    isWinningDouble = dice1 && dice2
+    console.log('Первый бросок: ' + dice1)
+    console.log('Второй бросок: ' + dice2)
 }
-*/
+
+if (isWinningDouble) {
+    console.log('Выигрышный дубль!')
+} else {
+    console.log('Не выигрышный дубль.')
+}
